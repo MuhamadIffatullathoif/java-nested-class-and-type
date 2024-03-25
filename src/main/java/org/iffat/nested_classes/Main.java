4bbced013d397a9c5363e20df1e3fae21c28a1fb
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-03-25 10:38:35 +0700

    03. Inner classes

--- src/main/java/org/iffat/nested_classes/Main.java
+++ src/main/java/org/iffat/nested_classes/Main.java
@@ -2,6 +2,7 @@ package org.iffat.nested_classes;
 
 import org.iffat.nested_classes.domain.Employee;
 import org.iffat.nested_classes.domain.EmployeeComparator;
+import org.iffat.nested_classes.domain.StoreEmployee;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +23,24 @@ public class Main {
         employees.sort(new Employee.EmployeeComparator<>("yearStarted").reversed());
 
         for (Employee e: employees) {
+            System.out.println(e);
+        }
+
+        System.out.println("Store Members");
+        List<StoreEmployee> storeEmployees = new ArrayList<>(List.of(
+                new StoreEmployee(10015,"Meg",2019,"Target"),
+                new StoreEmployee(10515,"Joe",2021,"Walmart"),
+                new StoreEmployee(10105,"Tom",2020,"Macys"),
+                new StoreEmployee(10215,"Marty",2018,"Walmart"),
+                new StoreEmployee(10322,"Bud",2016,"Target")
+        ));
+
+        // var genericEmployee = new StoreEmployee();
+        // var comparator = genericEmployee. new StoreComparator<>();
+        var comparator = new StoreEmployee(). new StoreComparator<>();
+        storeEmployees.sort(comparator);
+
+        for (StoreEmployee e: storeEmployees) {
             System.out.println(e);
         }
     }
