178e3ae1f194c0e0b0b04ddeca877f926d1b5b4f
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-03-25 12:17:51 +0700

    06. Local classes

--- src/main/java/org/iffat/nested_classes/burger/Store.java
+++ src/main/java/org/iffat/nested_classes/burger/Store.java
@@ -1,12 +1,18 @@
 package org.iffat.nested_classes.burger;
 
+import org.iffat.nested_classes.domain.Employee;
+import org.iffat.nested_classes.domain.StoreEmployee;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Store {
     public static void main(String[] args) {
         Meal regularMeal = new Meal();
-        regularMeal.addToppings("Ketchup","Mayo","Bacon","Cheddar");
+        regularMeal.addToppings("Ketchup", "Mayo", "Bacon", "Cheddar");
         System.out.println(regularMeal);
 
         Meal USRegularMeal = new Meal(0.65);
-        System.out.println(USRegularMeal    );
+        System.out.println(USRegularMeal);
     }
 }
