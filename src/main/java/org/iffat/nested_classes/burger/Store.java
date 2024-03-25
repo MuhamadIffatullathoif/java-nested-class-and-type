5f63fa31f6b045d140717804e949471ca2bc5747
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-03-25 11:41:10 +0700

    05. Inner classes Challenge

--- src/main/java/org/iffat/nested_classes/burger/Store.java
+++ src/main/java/org/iffat/nested_classes/burger/Store.java
@@ -3,6 +3,7 @@ package org.iffat.nested_classes.burger;
 public class Store {
     public static void main(String[] args) {
         Meal regularMeal = new Meal();
+        regularMeal.addToppings("Ketchup","Mayo","Bacon","Cheddar");
         System.out.println(regularMeal);
 
         Meal USRegularMeal = new Meal(0.65);
